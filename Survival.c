384e4dd942d76c5b20f7e065609538d356a3c9c9
TactileHornet61 <54224911+GameHot@users.noreply.github.com>
2025-02-14 18:28:22 -0500

    Update Survival.c
    Created a prototype of how to change coordinates( not perfect)

--- Survival.c
+++ Survival.c
@@ -15,6 +15,8 @@ typedef struct Item{
 	int total;
 }Item;
 
+
+
 #define MAX_INVENTORY 10
 typedef struct Person{
 	int health;
@@ -25,8 +27,8 @@ typedef struct Person{
 }Person;
 
 
+Person  init_player(){
 
- Person init_player(){
 	//first start by intitializing a Person called player
 	Person player;
 	player.health = MAX;
@@ -35,19 +37,64 @@ typedef struct Person{
 	player.coordinates.y = 0;
 	player.inventory_count = 0;
  
+       
 	//tells us that inventory is empty at the start of the game
-	for(int i = 0 ; i < MAX_INVENTORY; i++){
+	for(int i; i < MAX_INVENTORY; i++){
 		player.inventory[i].name = '\0';
 		player.inventory[i].total = 0;
 	}
 	return player;
 }
 
+
+ Person new_coordinates(Person *player){
+	
+
+	char rl;  //stands for right or left
+	char ud; // stand for up or down
+	printf("Would you like to go left or right r/l:  ");
+	scanf(" %c", &rl);
+	printf("\n");
+        printf("Would you like to go up or down u/d:  ");
+        scanf(" %c", &ud);
+	printf("\n");
+
+
+	if(rl == 'r'  && ud == 'u'){
+		(*player).coordinates.x += 1;
+		(*player).coordinates.y += 1;
+
+	} else if(rl == 'l' && ud == 'd'){
+		(*player).coordinates.x -= 1;
+		(*player).coordinates.y -= 1;
+
+	} else if(rl == 'l' && ud == 'u'){
+		(*player).coordinates.x -= 1;
+		(*player).coordinates.y += 1;
+
+	} else if(rl =='r'  && ud == 'd'){
+		(*player).coordinates.x += 1;
+		(*player).coordinates.y -= 1;
+	}else{
+		printf("no changes\n");
+	}
+
+
+
+
+	printf("Your new coordinates are (%d,%d)\n",(*player).coordinates.x, (*player).coordinates.y);
+	return *player;
+}
+
+
+
+
+
 int main(){
-  Person player = init_player();
+  Person player =  init_player();
   printf("Player Health is %d and player hunger is %d\n", player.health, player.hunger);
   printf("PLayer coordinates is (%d,%d), and inventory is currently at 0\n", player.coordinates.x, player.coordinates.y);
   printf("Welcome to the game, Player One.\n");
-
+  
+  player  = new_coordinates(&player);
 }
-
