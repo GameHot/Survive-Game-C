cb3847932361e2bf4f674e889ec6798d2df0d80d
TactileHornet61 <54224911+GameHot@users.noreply.github.com>
2025-02-15 21:41:04 -0500

    Update Survival.c
    replaced if statements with switch statements for better handling

--- Survival.c
+++ Survival.c
@@ -9,6 +9,7 @@ typedef struct Coord {
 	int y;
 }Coord;
 
+
 typedef struct Item{
 	char name;
 	int total;
@@ -26,9 +27,7 @@ typedef struct Person{
 }Person;
 
 
-
-Person init_player(){
-
+Person  init_player(){
 	//first start by intitializing a Person called player
 	Person player;
 	player.health = MAX;
@@ -36,67 +35,81 @@ Person init_player(){
 	player.coordinates.x = 0;
 	player.coordinates.y = 0;
 	player.inventory_count = 0;
- 
-
-
-
+        
 	//tells us that inventory is empty at the start of the game
-	for(int i = 0; i < MAX_INVENTORY; i++){
+	for(int i; i < MAX_INVENTORY; i++){
 		player.inventory[i].name = '\0';
 		player.inventory[i].total = 0;
 	}
 	return player;
 }
 
-
- Person new_coordinates(Person *player){
+ void  new_coordinates(Person *player){
 	
-
+	//DECLARES RL VARIABLES;
 	char rl;  //stands for right or left
 	char ud; // stand for up or down
+	
 	printf("Would you like to go left or right r/l:  ");
 	scanf(" %c", &rl);
 	printf("\n");
-        printf("Would you like to go up or down u/d:  ");
+        
+	printf("Would you like to go up or down u/d:  ");
         scanf(" %c", &ud);
 	printf("\n");
 
-
-	if(rl == 'r'  && ud == 'u'){
-		(*player).coordinates.x += 1;
-		(*player).coordinates.y += 1;
-
-	} else if(rl == 'l' && ud == 'd'){
-		(*player).coordinates.x -= 1;
-		(*player).coordinates.y -= 1;
-
-	} else if(rl == 'l' && ud == 'u'){
-		(*player).coordinates.x -= 1;
-		(*player).coordinates.y += 1;
-
-	} else if(rl =='r'  && ud == 'd'){
-		(*player).coordinates.x += 1;
-		(*player).coordinates.y -= 1;
-	}else{
-		printf("no changes\n");
+//	if(rl == 'r'  && ud == 'u'){
+//		(*player).coordinates.x += 1;
+//		(*player).coordinates.y += 1;
+//
+//	} else if(rl == 'l' && ud == 'd'){
+//		(*player).coordinates.x -= 1;
+//		(*player).coordinates.y -= 1;
+//
+//	} else if(rl == 'l' && ud == 'u'){
+//		(*player).coordinates.x -= 1;
+//		(*player).coordinates.y += 1;
+//
+//	} else if(rl =='r'  && ud == 'd'){
+//		(*player).coordinates.x += 1;
+//		(*player).coordinates.y -= 1;
+//      }else{
+//		printf("no changes\n");
+//	}
+        
+	switch(rl) {
+		case 'r':
+			(*player).coordinates.x += 1;
+			break;
+		case 'l':
+			(*player).coordinates.x -= 1;
+			break;
+		default:
+			printf("No change in x coordinates\n");
+			break;
 	}
 
+	switch(ud) {
+		case 'u':
+			(*player).coordinates.y += 1;
+			break;
+		case 'd':
+			(*player).coordinates.y -= 1;
+			break;
+		default:
 
-
-
+			printf("No change in y-coordinates\n");
+		        break;
+	}
 	printf("Your new coordinates are (%d,%d)\n",(*player).coordinates.x, (*player).coordinates.y);
-	return *player;
 }
 
 
-
-
-
 int main(){
+//The following is test code;
   Person player =  init_player();
   printf("Player Health is %d and player hunger is %d\n", player.health, player.hunger);
   printf("PLayer coordinates is (%d,%d), and inventory is currently at 0\n", player.coordinates.x, player.coordinates.y);
   printf("Welcome to the game, Player One.\n");
-  
-  player  = new_coordinates(&player);
+  new_coordinates(&player);
 }
